c21fc7e3c7ea5ceb492a8d67fa11b8a3d7bc34c2
CaioRobertoo <caiorscampos@gmail.com>
2025-10-24 12:19:09 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,22 @@
+public class Funcionario {
+
+    private String nome;
+    private String cpf;
+
+    public Funcionario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public double calcularSalario() {
+        return 1500.00;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
